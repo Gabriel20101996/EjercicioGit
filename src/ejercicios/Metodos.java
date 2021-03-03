9848374eaeb4fa19b9a1ca13083719433e467b89
Gabriel Canda <gabriel.candagcb9@gmail.com>
2021-03-03 09:47:36 +0100

    Creacion metodoConcatenacion

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -4,7 +4,12 @@ public class Metodos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println(metodoConcatenacion("Hola"," Luis"));
 	}
-
+	
+	static String metodoConcatenacion(String cadena1, String cadena2) {
+		String cadena3=cadena1+cadena2;
+		return cadena3;	
+	}
+	
 }
