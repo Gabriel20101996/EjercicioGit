00af413160194a05e7031c2ea539cf1423aa020e
Gabriel Canda <gabriel.candagcb9@gmail.com>
2021-03-03 10:11:53 +0100

    Creacion metodoCaracter

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -5,11 +5,19 @@ public class Metodos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(metodoConcatenacion("Hola"," Luis"));
+System.out.println(metodoCaracter('1'));
 	}
 	
 	static String metodoConcatenacion(String cadena1, String cadena2) {
 		String cadena3=cadena1+cadena2;
 		return cadena3;	
 	}
+	
+	static char metodoCaracter(int numero) {
+		char caracter=(char)(numero+'0');
+		return caracter;
+	}
+	
+	
 	
 }
